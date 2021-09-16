4e0543e6d5e3061c49046dd774a51824d9bc8c36
Matteo92i <matteo.iovane@gmail.com>
2021-09-16 14:12:42 +0200

    primo punto

--- js/script.js
+++ js/script.js
@@ -3,3 +3,9 @@
 // Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato sarà aggiunto alla lista
 
 
+let app = new Vue({
+    el: '#app',
+    data: {
+      listaSpesa: ["pollo", "riso basmati", "fiocchi di latte", "pane integrale", "fesa", "yogurt greco", "frutta secca"]
+    }
+  })
