f5210a281705f1af388f4a3c4dd8464e21d0e961
Matteo92i <matteo.iovane@gmail.com>
2021-09-16 13:55:07 +0200

    inizio

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// Stampare in pagina un item per ogni elemento contenuto in un array
+// Ogni item ha una icona "x" associata: cliccando su di essa, l'item viene rimosso dalla lista
+// Predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato sarà aggiunto alla lista
+
+
